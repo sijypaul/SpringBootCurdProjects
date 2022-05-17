b8d32d1db4e9c552aee2e6e30d04936054a1208d
sijypaul <104199522+sijypaul@users.noreply.github.com>
2022-05-17 12:43:01 +0530

    updated

--- src/main/java/net/javaguides/springboot/springbootcurdrestfulwebservices/Application.java
+++ src/main/java/net/javaguides/springboot/springbootcurdrestfulwebservices/Application.java
@@ -6,7 +6,7 @@ import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
 
-@SpringBootApplication
+//@SpringBootApplication
 public class Application {
 
 	@Bean
